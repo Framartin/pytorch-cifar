64e5e374eb19b7fdbd19bbef936dde57b5a3423b
Framartin <1850174+Framartin@users.noreply.github.com>
2023-02-27 19:24:44 +0100

    deactiavte converter

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ if device == 'cuda':
     # net = torch.nn.DataParallel(net)
     cudnn.benchmark = True
 
-net = extend(net, use_converter=True)
+net = extend(net)  # use_converter=True
 
 if args.resume:
     # Load checkpoint.
